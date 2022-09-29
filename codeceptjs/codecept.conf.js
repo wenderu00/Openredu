27da9a82a31528bf2ced5d708357f0e4f391a946
wenderu00 <mwmcjr@gmail.com>
2022-09-29 18:23:31 -0300

    create a new codecept project

---
+++ codeceptjs/codecept.conf.js
@@ -0,0 +1,56 @@
+const dotenv = require('dotenv');
+
+let env = dotenv.config();
+
+exports.config = {
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: process.env.BASE_URL,
+      show: process.env.SHOW_BROWSER,
+      windowSize: process.env.WINDOW_SIZE
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  timeout: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    retryFailedStep: {
+      enabled: false
+    },
+    retryTo: {
+      enabled: true
+    },
+    eachElement: {
+      enabled: true
+    },
+    pauseOnFail: {}
+  },
+  stepTimeout: 0,
+  stepTimeoutOverride: [{
+      pattern: 'wait.*',
+      timeout: 0
+    },
+    {
+      pattern: 'amOnPage',
+      timeout: 0
+    }
+  ],
+  tests: './*_test.js',
+  name: 'codeceptjs'
+}
