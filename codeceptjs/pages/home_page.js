a6c65fb8b38a8cb0d6f9edada808af61a6acb80e
wenderu00 <mwmcjr@gmail.com>
2022-10-01 11:29:33 -0300

    complete home page class

--- codeceptjs/pages/home_page.js
+++ codeceptjs/pages/home_page.js
@@ -5,10 +5,18 @@ class HomePage extends BasePage {
     constructor(){
         super();
         this.iconOpenreduLink = '//a[@title="Openredu"]';
+        this.beginLinkActive = '//a[@class="nav-global-button nav-global-button-active simple-navigation-active-leaf" and @title="Início"]';
+        this.teachLink = '//a[@title="Ensine"]';
+        this.environmentsLink ='//a[@title="Ambientes"]';
+        this.appsLink = '//a[@title="Aplicativos"]';
     }
 
     async isHomePage(){
         I.waitForElement(this.iconOpenreduLink,this.timeout);
+        I.waitForElement(this.beginLinkActive,this.timeout);
+        I.waitForElement(this.teachLink,this.timeout);
+        I.waitForElement(this.teachLink,this.timeout);
+        I.waitForElement(this.appsLink,this.timeout);
     }
 }
 
